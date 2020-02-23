ead3318b46cf55d2164624a4053cde9c07c67770
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-02-23 15:47:04 +0000

    [Finish]Bookers2-debug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,25 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+
+  def new
+    @book = Book.new
+  end
 
   def show
-  	@book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
+    @book = Book.new
   	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
   end
 
   def create
-  	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+  	@book = current_user.books.build(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
   	if @book.save #入力されたデータをdbに保存する。
-  		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
+  		redirect_to book_path(@book.id), notice: "successfully created book!"#保存された場合の移動先を指定。
   	else
   		@books = Book.all
   		render 'index'
@@ -19,13 +27,9 @@ class BooksController < ApplicationController
   end
 
   def edit
-  	@book = Book.find(params[:id])
   end
 
-
-
   def update
-  	@book = Book.find(params[:id])
   	if @book.update(book_params)
   		redirect_to @book, notice: "successfully updated book!"
   	else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
@@ -33,16 +37,26 @@ class BooksController < ApplicationController
   	end
   end
 
-  def delete
-  	@book = Book.find(params[:id])
-  	@book.destoy
+  def destroy
+  	@book.delete
   	redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
 
+  def correct_user
+    @user = @book.user
+    unless @user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:user_id,:title,:body)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-	before_action :baria_user, only: [:update]
+  before_action :authenticate_user!, onry: [:index,:show,:new,:edit,:create,:update]
+  before_action :set_user, only: [:edit, :show, :update]
+  before_action :baria_user, only: [:update,:edit]
 
   def show
-  	@user = User.find(params[:id])
   	@books = @user.books
   	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
   end
@@ -10,30 +11,34 @@ class UsersController < ApplicationController
   def index
   	@users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
   	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+  end
 
   def edit
-  	@user = User.find(params[:id])
   end
 
   def update
-  	@user = User.find(params[:id])
-  	if @user.update(user_params)
-  		redirect_to users_path(@user), notice: "successfully updated user!"
-  	else
-  		render "show"
-  	end
+   if @user.update(user_params)
+    redirect_to user_path(@user.id), notice: "successfully updated user!"
+  else
+    render "edit"
   end
+end
 
-  private
-  def user_params
-  	params.require(:user).permit(:name, :introduction, :profile_image)
-  end
+private
+def user_params
+ params.require(:user).permit(:name, :introduction, :profile_image)
+end
+
+def set_user
+  @user = User.find(params[:id])
+end
 
   #url直接防止　メソッドを自己定義してbefore_actionで発動。
-   def baria_user
+  def baria_user
   	unless params[:id].to_i == current_user.id
   		redirect_to user_path(current_user)
   	end
-   end
+  end
 
 end
+
