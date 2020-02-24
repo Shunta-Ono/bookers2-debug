d8ea7ef054d24dd817db302455c572af5dce28a4
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-02-24 09:06:21 +0000

    いいね機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
   get "home/about" => "homes#about"
   root :to => 'homes#top'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
